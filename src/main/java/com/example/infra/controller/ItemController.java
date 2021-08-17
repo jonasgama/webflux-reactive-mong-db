2b51e5ac410d8c3811a16c64b0ecf4e33c9bf703
Jonas Gama <jonasgamaifs@gmail.com>
2021-08-17 14:19:11 -0300

    Implements Flux to get all items from the database

---
+++ src/main/java/com/example/infra/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.example.infra.controller;
+
+import com.example.infra.document.Item;
+import com.example.infra.gateway.ItemGateway;
+import com.example.infra.repository.ItemRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/")
+public class ItemController {
+
+    @Autowired
+    private ItemGateway gateway;
+
+    @GetMapping("v1")
+    public Flux<Item> getAllItems(){
+        return gateway.get();
+    }
+
+
+}
