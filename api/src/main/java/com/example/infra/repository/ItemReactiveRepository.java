36d2ded7a7085994544481602356ceb473ffe116
Jonas Gama <jonasgamaifsp@gmail.com>
2021-09-27 16:46:12 -0300

    initialize reactive mongo repository

---
+++ api/src/main/java/com/example/infra/repository/ItemReactiveRepository.java
@@ -0,0 +1,14 @@
+package com.example.infra.repository;
+
+import com.example.infra.document.Item;
+import com.example.infra.document.ItemCapped;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ItemReactiveRepository extends ReactiveMongoRepository<ItemCapped, String> {
+
+    @Tailable
+    Flux<Item> findItemsBy();
+
+}
