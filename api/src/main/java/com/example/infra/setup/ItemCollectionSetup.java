b583d97248b6f3d308048d95623ed76ea5fab4c4
Jonas Gama <jonasgamaifsp@gmail.com>
2021-09-28 09:22:09 -0300

    creating auto bulking data to further tests.

--- api/src/main/java/com/example/infra/setup/ItemCollectionSetup.java
+++ api/src/main/java/com/example/infra/setup/ItemCollectionSetup.java
@@ -1,12 +1,15 @@
 package com.example.infra.setup;
 
 import com.example.infra.document.ItemCapped;
+import com.example.infra.repository.ItemReactiveRepository;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.CollectionOptions;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 @Component
 @Profile("!test")
@@ -15,9 +18,21 @@ public class ItemCollectionSetup implements CommandLineRunner {
   @Autowired
   private ReactiveMongoOperations mongo;
 
+  @Autowired
+  private ItemReactiveRepository repo;
+
   @Override
   public void run(String... args) throws Exception {
     createCappedCollection();
+    infiniteCappedInsertion();
+
+  }
+
+  private void infiniteCappedInsertion() {
+    repo.deleteAll();
+    repo.insert(  Flux.interval(Duration.ofSeconds(2))
+          .map(i -> new ItemCapped(null, "ITEM", 1.0+i)) )
+        .subscribe(newItem -> System.out.println(newItem));
   }
 
   private void createCappedCollection(){
